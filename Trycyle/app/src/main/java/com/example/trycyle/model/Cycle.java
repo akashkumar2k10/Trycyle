da1877abb4834fca204e266eb0a75e0e9de844a7
Akash Kumar <akashkumar2k10@gmail.com>
2020-11-04 00:04:10 +0530

    database added. cycle Transaction with randomString added to firebase Timer page is created

--- Trycyle/app/src/main/java/com/example/trycyle/model/Cycle.java
+++ Trycyle/app/src/main/java/com/example/trycyle/model/Cycle.java
@@ -1,18 +1,25 @@
 package com.example.trycyle.model;
 
-public class Cycle {
+import java.io.Serializable;
+
+public class Cycle implements Serializable {
     String name;
     String code;
 
     String status;  //0 mean free and 1 mean taken
 
+    public Cycle() {
+    }
 
-
-    public Cycle(String name, String code,String status) {
+    public Cycle(String name, String code, String status) {
         this.name = name;
         this.code = code;
         this.status=status;
 
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
     }
 
     public String getName() {
